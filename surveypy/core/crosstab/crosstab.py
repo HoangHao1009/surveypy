73d96d38b2159c7cceebed91890bf0fcb276ee60
hoanghao <hahoanghao1009@gmail.com>
2024-10-07 12:21:17 +0700

    .

--- surveypy/core/crosstab/crosstab.py
+++ surveypy/core/crosstab/crosstab.py
@@ -299,7 +299,12 @@ def _sm_ctab(
         pv.loc[:, :] = 0
 
     pv.rename_axis(index=['row', 'row_value'], columns=['col', 'col_value'], inplace=True)
-    total_df = pv.loc[[total_label],:]
+    try:
+        total_df = pv.loc[[total_label],:]
+    except:
+        print(base.code)
+        print(target.code)
+        total_df = pv.loc[[total_label],:]
 
     pv = pv.loc[~pv.index.get_level_values(0).isin([total_label])]
     if sig:
