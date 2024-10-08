3a4b45810f0f97ce20d6db41111aab8fc361f3cb
hoanghao <hahoanghao1009@gmail.com>
2024-10-08 09:19:36 +0700

    .

--- surveypy/core/crosstab/crosstab.py
+++ surveypy/core/crosstab/crosstab.py
@@ -51,7 +51,7 @@ class CrossTab(BaseModel):
         def parallel_process(response_pairs):
             with ProcessPoolExecutor(max_workers=multiprocessing.cpu_count()) as process_executor:
                 # Sử dụng đa tiến trình để xử lý các cặp
-                results = list(process_executor.map(process_pair, response_pairs))
+                results = list(process_executor.map(process_pair, response_pairs, self.bases, self.targets, self.config))
             return dict(results)
 
         # Sử dụng kết hợp đa tiến trình và đa luồng
